9b3b089ec8d127c7ac8f03c9e0927cd0872d1c40
ADRIANO LIMA <90012808+AdrianoVK@users.noreply.github.com>
2025-03-17 21:12:37 -0300

    Update Movendo_Arquivos.py

--- Movendo_Arquivos.py
+++ Movendo_Arquivos.py
@@ -2,7 +2,7 @@ from datetime import datetime
 from pathlib import Path 
 from shutil import move, rmtree, copy 
 import os   
-import time 
+import time  
   
 path_base = r'CAMINHO DA PASTA DOWNLOADS'  
 destino = Path(r'CAMINHO DA PASTA DE DESTINO') # No meu caso a pasta es ta alocada na Rede (outro servidor)
